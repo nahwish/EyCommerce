0284a95e1e9024cbb892eb8d6ea99c77ecb494bf
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-08-10 17:09:35 -0300

    refactor fetch in home

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,29 +1,44 @@
-import { createContext,useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const ShoppingCartContext = createContext();
 
-export const ShoppingCartProvider = ({children}) => {
+export const ShoppingCartProvider = ({ children }) => {
   // Increment quantity
-  const [ count, setCount ] = useState( 0 );
+  const [count, setCount] = useState(0);
   // Product detail
   const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
   const openProductDetail = () => setIsProductDetailOpen(true);
   const closeProductDetail = () => setIsProductDetailOpen(false);
-  // Checkout 
-    const [isCheckoutMenuOpen, setIsCheckoutMenuOpen] = useState(false);
-    const openCheckoutMenu = () => setIsCheckoutMenuOpen(true);
-    const closeCheckoutMenu = () => setIsCheckoutMenuOpen(false);
+  // Checkout
+  const [isCheckoutMenuOpen, setIsCheckoutMenuOpen] = useState(false);
+  const openCheckoutMenu = () => setIsCheckoutMenuOpen(true);
+  const closeCheckoutMenu = () => setIsCheckoutMenuOpen(false);
   //ShowProduct
-  const [ productToShow,setProductToShow ] = useState({})
+  const [productToShow, setProductToShow] = useState({});
   //ShoppingCart
   const [cartProduct, setCartProduct] = useState([]);
-  
+
   // Order
   const [order, setOrder] = useState([]);
+  //get Products
+  const [items, setItems] = useState([]);
+  const API = "https://api.escuelajs.co/api/v1/products";
+  useEffect(() => {
+    try {
+      async function fetchData(URL) {
+        let result = await fetch(URL);
+        let data = await result.json();
+        setItems(data);
+      }
+      fetchData(API);
+    } catch (error) {
+      console.log("Error ->", error);
+    }
+  }, []);
 
   const increment = () => {
-    setCount( count + 1 )
-  }
+    setCount(count + 1);
+  };
 
   const contextValue = {
     increment,
@@ -40,9 +55,12 @@ export const ShoppingCartProvider = ({children}) => {
     isCheckoutMenuOpen,
     setOrder,
     order,
+    items,
+    setItems,
   };
   return (
-    <ShoppingCartContext.Provider value={contextValue }>
+    <ShoppingCartContext.Provider value={contextValue}>
       {children}
     </ShoppingCartContext.Provider>
-  )}
+  );
+};
