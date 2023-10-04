2ada1fc01460fe26790673e46b31fe8ac49c69b4
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-10-04 11:07:41 -0300

    create user

--- src/Pages/Signin/index.jsx
+++ src/Pages/Signin/index.jsx
@@ -1,11 +1,12 @@
 import Layout from "../../Components/Layout";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCartContext } from "../../Context/cartContext";
 
 
 function Signin() {
   const context = useContext(ShoppingCartContext);
+  const [view, setView] = useState('user-info');
 
   // Account
   const account = localStorage.getItem('account');
@@ -14,35 +15,51 @@ function Signin() {
   const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
   const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true;
   const hasUserAnAccount = !noAccountInLocalState || !noAccountInLocalStorage;
+
+  const renderLogIn = () =>{
+    return (
+      <div className="flex flex-col w-80">
+        <p>
+          <span className="font-light text-sm">Email:</span>
+          <span>{parsedAccount?.email}</span>
+        </p>
+        <p>
+          <span className="font-light text-sm">Password:</span>
+          <span>{parsedAccount?.password}</span>
+        </p>
+        <Link to="/">
+          <button
+            className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2"
+            disabled={!hasUserAnAccount}
+          >
+            Log in
+          </button>
+        </Link>
+        <div className="text-center">
+          <a className="font-light text-xs underline-offset-4" href="/">
+            Forgot my passward
+          </a>
+        </div>
+        <button
+          className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
+          onClick={() => setView("create-user-info")}
+          disabled={hasUserAnAccount}
+        >
+          Sign up
+        </button>
+      </div>
+    );
+  }
+  const renderCreateUserInfo = () =>{
+    // TODO
+  }
+  const renderView = () =>view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn();
+
   return (
     <>
       <Layout>
         <h1 className="font-medium text-xl mb-6 w-80">Welcome</h1>
-        <div className="flex flex-col w-80">
-          <p>
-            <span className="font-light text-sm">Email:</span>
-            <span>{parsedAccount?.email}</span>
-          </p>
-          <Link to="/">
-            <button className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2"
-            disabled={!hasUserAnAccount}
-            >
-              Log in
-            </button>
-          </Link>
-          <div className="text-center">
-            <a 
-            className="font-light text-xs underline-offset-4"
-            href="/">
-              Forgot my passward
-            </a>
-          </div>
-          <button className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
-          disabled={hasUserAnAccount}
-          >
-            Sign ip
-          </button>
-        </div>
+        {renderView()}
       </Layout>
     </>
   );
