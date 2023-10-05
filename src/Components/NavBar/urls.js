839618798c3433e86fa1d3e75510f70fc438faab
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-10-05 12:52:36 -0300

    jsdoc

--- src/Components/NavBar/urls.js
+++ src/Components/NavBar/urls.js
@@ -1,12 +1,18 @@
 export const directionLink = [
-  {name:"Shopi",url:"/"}, 
-  {name:"All",url:"/"}, 
-  {name:"Shoes",url:"/shoes"}, 
-  {name:"Electronics",url:"/electronics"}, 
-  {name:"Toys",url:"/toys"}];
+  { name: "All", url: "/", category: "" },
+  { name: "Others", url: "/others", category: "others" },
+  { name: "Shoes", url: "/shoes", category: "clothes" },
+  { name: "Electronics", url: "/electronics", category: "electronics" },
+];
 
 export  const activeLinkAcount = [
   { name: "My Orders", url: "/my-orders" },
   { name: "My Account", url: "/my-account" },
   { name: "Sign In", url: "/sign-in" },
+];
+
+export const signInLinks = [
+  { name: "My Orders", url: "/my-orders" },
+  { name: "My Account", url: "/my-account" },
+  { name: "Sign Out", url: "/sign-in" },
 ];
